7b1f717bb1f75c57f72a8e9161befc68517b7e24
sownyluv <snowyfe@proton.me>
2024-09-09 10:35:44 -0500

    added my idea

--- src/tools/instance.rs
+++ src/tools/instance.rs
@@ -3,4 +3,10 @@
 pub fn instance(usermessage: &str) {
     let _content: [&str; 1] = ["You are a helpful assistant."];
     println!("{:?}", usermessage)
+    /*
+    [ I D E A S ]
+    1. Process what the user said via usermessage
+    2. Machine gives it's own iinterp of what the user said
+    :3
+     */
 }
